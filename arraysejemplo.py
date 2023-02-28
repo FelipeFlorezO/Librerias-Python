e8fade2b802de98ab742f86fdfabaab2455b90bf
Felipe Florez Olivera <114842657+FelipeFlorezO@users.noreply.github.com>
2023-02-28 16:22:50 -0500

    Add files via upload

---
+++ arraysejemplo.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+lista = [1,2,3,4,5,6,7,8,9]
+
+arr = np.array(lista)
+
+type(arr)
+
+matriz = [[1,2,3],[4,5,6],[7,8,9]]
+matriz = np.array(matriz)
+matriz
+
+
+arr
+arr[1] + arr[5]
+
+
+matriz
+matriz[0]
+matriz[0,2]
+
+arr
+arr[1:6]
+arr[:6]
+arr[2:]
+arr
+arr[::3]
+arr[-1]
+matriz
+matriz[1:]
+matriz[1:,0:2]
